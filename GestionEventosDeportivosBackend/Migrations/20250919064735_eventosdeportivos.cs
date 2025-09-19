168b6b663c84f726e9cbcedcb7b5eea9848e0a2e
Christian <cgmorales1280@gmail.com>
2025-09-19 01:55:22 -0500

    eva2

---
+++ GestionEventosDeportivosBackend/Migrations/20250919064735_eventosdeportivos.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionEventosDeportivos.Migrations
+{
+    /// <inheritdoc />
+    public partial class eventosdeportivos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
